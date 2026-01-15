4f800a3e2401f05eb79d2402e7d42476840f6cff
Gyztor Mizirath <44737827+Gyztor@users.noreply.github.com>
2026-01-15 14:01:42 -0600

    Modify entrypoint.sh to check BYPASS_DOWNLOADER
    Add condition to bypass downloader if specified

--- games/hytale/entrypoint.sh
+++ games/hytale/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 cd /home/container
 
 # If HYTALE_SERVER_SESSION_TOKEN isn't set, assume the user will log in themselves, rather than a host's GSP
-if [[ -z "$HYTALE_SERVER_SESSION_TOKEN" ]]; then
+if [["${BYPASS_DOWNLOADER}" == false ]]; then
 	./hytale-downloader/hytale-downloader-linux -patchline "$HYTALE_PATCHLINE" -download-path HytaleServer.zip -credentials-path ./.hytale-downloader-credentials.json 
 
 	unzip -o HytaleServer.zip -d .
