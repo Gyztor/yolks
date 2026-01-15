4d53bd74c80c61c29ce473d71812d11451801cc6
Gyztor Mizirath <44737827+Gyztor@users.noreply.github.com>
2026-01-15 14:30:13 -0600

    Update entrypoint.sh to check for HytaleServer.zip

--- games/hytale/entrypoint.sh
+++ games/hytale/entrypoint.sh
@@ -10,7 +10,7 @@ if [ "${BYPASS_DOWNLOADER}" == "0" ]; then
 	unzip -o HytaleServer.zip -d .
 
 	rm -f HytaleServer.zip
-elif [ "${BYPASS_DOWNLOADER}" == "1" ]; then
+elif [[ -f "HytaleMount/HytaleServer.zip" ]]; then
 	unzip -o HytaleMount/HytaleServer.zip -d .
 	echo "Unzipped HytaleServer"
 elif [[ -f "HytaleMount/Assets.zip" ]]; then
